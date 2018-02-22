8c5334daaed8129e390fd6c4afa38730d0e35dd8
yaojinhui <yaojinhui1993@gmail.com>
2018-02-22 16:20:46 +0800

    Episode 15. Eloquent Relationships and Comments.

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,19 @@
-@extends('layouts.master') 
-@section('content')
-    <h1>{{ $post->title }}</h1>
+@extends('layouts.master')
 
-    {{ $post->body }}
+@section('content')
+<h1>{{ $post->title }}</h1>
+
+{{ $post->body }}
+
+<hr>
+<ul class="list-group">
+    @foreach ($post->comments as $comment)
+    <li class="list-group-item">
+        <strong>{{ $comment->created_at->diffForHumans() }}:</strong>
+        {{ $comment->body }}
+    </li>
+    @endforeach
+</ul>
 
 
 @endsection
